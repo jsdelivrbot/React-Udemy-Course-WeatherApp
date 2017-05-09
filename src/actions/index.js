db2bb669641e2acd11c4131deeb3a46bb8af794c
Andrey Olkhovsky <andrej.olkhovsky@gmail.com>
2017-05-09 14:06:09 +0300

    Implemented the fetchWeather() action producer. Installed axios lib for ajax requests.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '1513b2322434897b7d858208aa0d046e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    // returns an Action
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
